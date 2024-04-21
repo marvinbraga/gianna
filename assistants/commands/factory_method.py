aaf26192f763009c89cf2d8a53efee2ec437d786
Marcus Braga <mvbraga@gmail.com>
2024-04-21 08:44:52 -0300

    Commands.

--- assistants/commands/factory_method.py
+++ assistants/commands/factory_method.py
@@ -5,3 +5,8 @@ def get_command(activation_key_word: str, **kwargs):
     command_factory = CommandRegister().get_factory(activation_key_word)
     cmd = command_factory.create(**kwargs)
     return cmd
+
+
+def list_commands():
+    commands = CommandRegister.list()
+    return [activation_key for _, activation_key in commands]
